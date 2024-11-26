f2d3de565f4f134625649da50665c3e20d2d5e57
h1tax <115391278+z1rowww@users.noreply.github.com>
2024-11-26 21:15:26 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+import { curriedAdd } from './curriedAdd.js'
+import { curriedDomain } from './curriedDomain.js'
+import { modifyFunction } from './modifyFunction.js'
+import { outerFunction } from './nestedFunctions.js'
